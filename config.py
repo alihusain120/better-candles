2f54fe1ca30f2adbd75f4c1cdec15e3671023a4a
alihus <alihusain120@gmail.com>
2021-12-16 11:45:41 -0600

    template config file

---
+++ config.py
@@ -0,0 +1,10 @@
+'''
+IMPORTANT: Never upload API Keys on public platforms. Author is not responsible for misuse.
+'''
+
+APIKEY = '<Binance API Key>'
+APISECRET = '<Binance API Secret>'
+
+# Not Required for better-candles
+TESTKEY = '<Binance Testnet API Key>'
+TESTSECRET = '<Binance Testnet API Key>'
