4efdc8926f06f8b1a670a12d22992fed00113241
alihus <alihusain120@gmail.com>
2022-02-24 18:38:39 -0500

    TickBars supports multi tickers

--- TickBars.py
+++ TickBars.py
@@ -14,12 +14,13 @@ Streams trades from Binance API, aggregates and delivers as Tick bars (configura
 import time
 import os, sys
 
+
 from datetime import datetime
 from binance import ThreadedWebsocketManager
 import csv
 import config
 import logging as log
-#log.basicConfig(level=log.INFO)
+log.basicConfig(level=log.INFO)
 
 APIKEY = config.APIKEY
 APISECRET = config.SECRET
@@ -39,41 +40,45 @@ class TickBars:
         def __str__(self):
             return ",\n".join([f"{x}:{y}" for x, y in self.__dict__.items()])
 
-    def __init__(self, symbol: str='BTCUSDT', tick_threshold: int=1000, to_csv: bool=False, csv_filepath: str=''):
+    def __init__(self, symbol: str='BTCUSDT', tick_threshold: int=1000, to_csv: bool=False):
         # TODO: Add support for str[] of symbols in place of symbol parameter
 
         # self.last_time = time.time()
 
         self.twm = ThreadedWebsocketManager(api_key=APIKEY, api_secret=APISECRET)
 
-        self.SYMBOL = symbol
+        with open(os.path.join(sys.path[0], "ticks.txt"), "r") as f:
+            self.SYMBOLS = [line.strip() for line in f]
+
         self.TICK_THRESHOLD = tick_threshold # Candle for every {TICK_THRESHOLD} trades that occur on Binance for symbol
 
-        self.running_trades = []
-        self.candles = []
+        self.running_trades = {}
+        self.candles = {}
+        for s in self.SYMBOLS:
+            self.running_trades[s] = []
+            self.candles[s] = []
+
 
         self.TO_CSV = to_csv
-        self.CSV_PATH = None
         if to_csv:
-            if csv_filepath.endswith('.csv'):
-                self.CSV_PATH = csv_filepath
-            else:
-                # TODO: Edit for multiple symbol streaming functionality
-                self.CSV_PATH = f'TickBars_{symbol}.csv'
+            self.CSV_PATHS = {}
+            for s in self.SYMBOLS:
+                self.CSV_PATHS[s] = f'TickBars_{s}.csv'
 
 
-    def add_candle_to_csv(self, candle):
+    def add_candle_to_csv(self, candle, ticker):
+
         log.info(f"Adding candle to csv: {candle}")
 
-        if os.path.exists(self.CSV_PATH):
-            with open(self.CSV_PATH, "a+", newline='') as write_obj:
+        if os.path.exists(self.CSV_PATHS[ticker]):
+            with open(self.CSV_PATHS[ticker], "a+", newline='') as write_obj:
                 csv_writer = csv.DictWriter(write_obj, candle.__dict__.keys())
                 csv_writer.writerow(candle.__dict__)
             log.info(f"Added candle to existing csv.")
 
         else:
             # Write header for file creation
-            with open(self.CSV_PATH, "a+", newline='') as write_obj:
+            with open(self.CSV_PATHS[ticker], "a+", newline='') as write_obj:
                 csv_writer = csv.DictWriter(write_obj, candle.__dict__.keys())
                 csv_writer.writeheader()
                 csv_writer.writerow(candle.__dict__)
@@ -81,28 +86,43 @@ class TickBars:
 
     
     def create_candle(self, trades):
+        current_ticker = trades[0]['s']
+        log.info(f"Creating candle for {current_ticker}")
+
         prices = [float(x['p']) for x in trades]
         vol = sum([float(x['q']) for x in trades])
         candle = self.Candle(prices[0], max(prices), min(prices), prices[-1], vol, trades[0]['T'], trades[-1]['T'])
         
         if self.TO_CSV:
-            self.add_candle_to_csv(candle) 
+            self.add_candle_to_csv(candle, current_ticker) 
 
         # Feed candles to other scripts or trade logic from here
-        self.candles.append(candle) 
+        self.candles[current_ticker].append(candle) 
 
+    '''
+    {'stream': 'btcusdt@trade', 'data': {'e': 'trade', 'E': 1645736675086, 's': 'BTCUSDT', 't': 1268815872, 'p': '38400.00000000', 'q': '0.07257000', 'b': 9534389397, 'a': 9534372700, 'T': 1645736675085, 'm': False, 'M': True}}
+    '''
     
     def handle_message(self, msg):
-        self.running_trades.append(msg)
+        try:
+            current_ticker = msg['data']['s']
+        except:
+            print(f"Error processing, message: {msg}")
+            return
+        # assurance
+        assert(current_ticker in self.SYMBOLS)
 
-        if len(self.running_trades) >= self.TICK_THRESHOLD:
-            self.create_candle(self.running_trades)
-            self.running_trades.clear()
+        self.running_trades[current_ticker].append(msg['data'])
+
+        if len(self.running_trades[current_ticker]) >= self.TICK_THRESHOLD:
+            self.create_candle(self.running_trades[current_ticker])
+            self.running_trades[current_ticker].clear()
 
     def stream(self):
         print("Beginning Binance data stream. Ctrl-c to quit.")
         self.twm.start()
-        self.twm.start_trade_socket(callback=self.handle_message, symbol=self.SYMBOL)
+        streams = [f"{s.lower()}@trade" for s in self.SYMBOLS]
+        self.twm.start_multiplex_socket(callback=self.handle_message, streams=streams)
 
     def stop_stream(self):
         self.twm.stop()
@@ -116,3 +136,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
